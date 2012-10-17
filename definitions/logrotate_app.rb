4894437479e39684772ad8d941cf7476f5d084d4
Justin Witrick <justin.witrick@mailtrust.com>
2012-10-17 11:31:39 -0400

    [COOK-1338] Changed some of the tenary operators to '||=' per feedback

--- definitions/logrotate_app.rb
+++ definitions/logrotate_app.rb
@@ -22,8 +22,8 @@ define :logrotate_app, :enable => true, :frequency => "weekly", :template => "lo
 
   acceptable_options = ['missingok', 'compress', 'delaycompress', 'copytruncate', 'notifempty', 'delaycompress', 'ifempty', 'mailfirst', 'nocompress', 'nocopy', 'nocopytruncate', 'nocreate', 'nodelaycompress', 'nomail', 'nomissingok', 'noolddir', 'nosharedscripts', 'notifempty', 'sharedscripts']
   path = params[:path].respond_to?(:each) ? params[:path] : params[:path].split
-  create = params[:create] ? params[:create] : "644 root adm"
-  options_tmp =  params[:options] ? params[:options] : ["missingok", "compress", "delaycompress", "copytruncate", "notifempty"]
+  create = params[:create] ||= "644 root adm"
+  options_tmp = params[:options] ||= ["missingok", "compress", "delaycompress", "copytruncate", "notifempty"]
   options = options_tmp.respond_to?(:each) ? options_tmp : options_tmp.split
 
   if params[:enable]
