0e8cf7747933e72302c6a5738e09ab3b35d459a8
jtimberman <joshua@opscode.com>
2012-10-30 15:37:06 -0600

    [COOK-1830] - add test kitchen support

---
+++ test/kitchen/cookbooks/logrotate_test/files/default/tests/minitest/logrotate_app_test.rb
@@ -0,0 +1,40 @@
+#
+# Author:: Joshua Timberman <joshua@opscode.com>
+# Copyright:: Copyright (c) 2012, Opscode, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+require File.expand_path('../support/helpers', __FILE__)
+
+describe 'logrotate::logrotate_app' do
+  include Helpers::Logrotate
+
+  it 'creates a basic template for tomcat-myapp' do
+    file('/etc/logrotate.d/tomcat-myapp').must_match(/^\/var\/log\/tomcat\/myapp.log/)
+  end
+
+  describe 'creates a template for tomcat-myapp with multiple paths' do
+    let(:config) { file('/etc/logrotate.d/tomcat-myapp-multi-path') }
+    it { config.must_match(/^\/var\/log\/tomcat\/myapp.log/) }
+    it { config.must_match(/^\/opt\/local\/tomcat\/catalina\.out/) }
+  end
+
+  it 'creates a template for tomcat-myapp without the create line' do
+    file('/etc/logrotate.d/tomcat-myapp-no-create').wont_match(/  create/)
+  end
+
+  it 'creates a template from an alternative cookbook' do
+    file('/etc/logrotate.d/tomcat-myapp-alt-cookbook').must_match(/# From an alternate cookbook/)
+  end
+
+end
---
+++ test/kitchen/cookbooks/logrotate_test/recipes/logrotate_app.rb
@@ -0,0 +1,46 @@
+#
+# Cookbook Name:: logrotate_test
+# Recipe:: logrotate_app
+#
+# Copyright 2012, Opscode, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+include_recipe "logrotate::default"
+
+logrotate_app "tomcat-myapp" do
+  path "/var/log/tomcat/myapp.log"
+  frequency "daily"
+  rotate 30
+  create "644 root adm"
+end
+
+logrotate_app "tomcat-myapp-multi-path" do
+  path [ "/var/log/tomcat/myapp.log", "/opt/local/tomcat/catalina.out" ]
+  frequency "daily"
+  create "644 root adm"
+  rotate 7
+end
+
+logrotate_app "tomcat-myapp-no-create" do
+  path "/var/log/tomcat/myapp.log"
+  frequency "daily"
+  rotate 30
+end
+
+logrotate_app "tomcat-myapp-alt-cookbook" do
+  cookbook "logrotate_test"
+  path "/var/log/tomcat/myapp.log"
+  frequency "daily"
+  rotate 30
+end
