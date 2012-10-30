1238ebb474a873d223fae34df2a2a0aee0f558e0
jtimberman <joshua@opscode.com>
2012-10-30 17:41:49 -0600

    release v1.0.0

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
 description       "Installs logrotate package and provides a definition for logrotate configs"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "0.8.2"
+version           "1.0.0"
 
 recipe "logrotate", "Installs logrotate package"
 provides "logrotate_app"
