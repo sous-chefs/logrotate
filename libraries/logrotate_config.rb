8acdac48590c92a86938989e258e035495383f93
Greg Symons <gsymons@drillinginfo.com>
2013-02-25 14:42:00 -0600

    Move the node attribute handling into a library.

---
+++ libraries/logrotate_config.rb
@@ -0,0 +1,72 @@
+module CookbookLogrotate
+    DIRECTIVES = %w[
+        compress        copy        copytruncate    daily           dateext
+        delaycompress   ifempty     mailfirst       maillast        missingok
+        monthly         nocompress  nocopy          nocopytruncate  nocreate
+        nodelaycompress nodateext   nomail          nomissingok     noolddir
+        nosharedscripts noshred     notifempty      sharedscripts   shred
+        weekly          yearly
+    ]
+
+    VALUES = %w[ 
+        compresscmd    uncompresscmd  compressext    compressoptions
+        create         dateformat     include        mail
+        maxage         minsize        rotate         size           
+        shredcycles    start          tabooext
+    ]
+
+    SCRIPTS = [ 'firstaction', 'prerotate', 'postrotate', 'lastaction', ]
+
+    DIRECTIVES_AND_VALUES = DIRECTIVES + VALUES
+
+    class LogrotateConfiguration
+        attr_reader :directives, :values, :paths
+        
+        class << self
+            def from_hash hash
+                return LogrotateConfiguration.new hash
+            end
+            
+            def directives_from hash
+                hash.select { |k, v| DIRECTIVES.include?(k) && v }.keys
+            end
+            
+            def values_from hash
+                hash.select { |k| VALUES.include? k }
+            end
+
+            def paths_from hash
+                hash.select { |k| !(DIRECTIVES_AND_VALUES.include? k) }.inject({}) do | accum_paths, (path, config) |
+                    accum_paths[path] = {
+                        'directives' => directives_from(config),
+                        'values' => values_from(config),
+                        'scripts' => scripts_from(config)
+                    }
+
+                    accum_paths
+                end
+            end
+            
+            def scripts_from hash
+                defined_scripts = hash.select { |k| SCRIPTS.include? k }
+                defined_scripts.inject({}) do | accum_scripts, (script, lines) |
+                    if lines.respond_to? :join
+                        accum_scripts[script] = lines.join "\n"
+                    else
+                        accum_scripts[script] = lines
+                    end
+
+                    accum_scripts
+                end
+            end
+        end
+
+        private
+        
+        def initialize hash
+            @directives = LogrotateConfiguration.directives_from hash
+            @values = LogrotateConfiguration.values_from hash
+            @paths = LogrotateConfiguration.paths_from hash
+        end
+    end
+end
--- recipes/global.rb
+++ recipes/global.rb
@@ -16,80 +16,18 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+require 'pp'
 
 include_recipe 'logrotate::default'
 
-def directives 
-    %w[
-        compress        copy        copytruncate    daily           dateext
-        delaycompress   ifempty     mailfirst       maillast        missingok
-        monthly         nocompress  nocopy          nocopytruncate  nocreate
-        nodelaycompress nodateext   nomail          nomissingok     noolddir
-        nosharedscripts noshred     notifempty      sharedscripts   shred
-        weekly          yearly
-    ]
-end
-
-def values 
-    %w[ 
-        compresscmd    uncompresscmd  compressext    compressoptions
-        create         dateformat     include        mail
-        maxage         minsize        rotate         size           
-        shredcycles    start          tabooext
-    ]
-end
-
-def scripts
-    [ 'firstaction', 'prerotate', 'postrotate', 'lastaction', ]
-end
-
-def directives_and_values 
-    directives + values
-end
-
-def directives_from hash
-    hash.select { |k, v| directives.include?(k) && v }.keys
-end
-
-def values_from hash
-    hash.select { |k| values.include? k }
-end
-
-def scripts_from hash
-    defined_scripts = hash.select { |k| scripts.include? k }
-    defined_scripts.inject({}) do | accum_scripts, (script, lines) |
-        if lines.respond_to? :join
-            accum_scripts[script] = lines.join "\n"
-        else
-            accum_scripts[script] = lines
-        end
-
-        accum_scripts
-    end
-end
-
-def paths_from hash
-    hash.select { |k| !(directives_and_values.include? k) }.inject({}) do | accum_paths, (path, config) |
-        accum_paths[path] = {
-            'directives' => directives_from(config),
-            'values' => values_from(config),
-            'scripts' => scripts_from(config)
-        }
-
-        accum_paths
-    end
-end
-
-defined_directives = directives_from node['logrotate']['global'].to_hash
-defined_values = values_from node['logrotate']['global'].to_hash
-defined_paths = paths_from node['logrotate']['global'].to_hash
+parsedConfiguration = CookbookLogrotate::LogrotateConfiguration.from_hash node['logrotate']['global'].to_hash
 
 template '/etc/logrotate.conf' do
     source 'logrotate-global.erb'
     mode '644'
     variables({
-        :directives => defined_directives,
-        :values => defined_values,
-        :paths => defined_paths
+        :directives => parsedConfiguration.directives,
+        :values => parsedConfiguration.values,
+        :paths => parsedConfiguration.paths 
     })
 end
