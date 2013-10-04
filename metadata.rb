a395eb3cd04e15d49bb6ca1d1778126c2d802d61
Seth Vargo <sethvargo@gmail.com>
2013-10-04 18:29:49 -0400

    Version bump to v1.4.1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email  'cookbooks@opscode.com'
 license           'Apache 2.0'
 description       'Installs logrotate package and provides a definition for logrotate configs'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           '1.4.0'
+version           '1.4.1'
 
 recipe 'logrotate', 'Installs logrotate package'
 provides 'logrotate_app'
