31f1d2fd733e763b78f5310e9b9447b091ec99d4
Cam Cope <ccope@brightcove.com>
2014-06-10 02:21:57 -0400

    add serverspec for logrotate.d folder

--- test/integration/global/serverspec/global_spec.rb
+++ test/integration/global/serverspec/global_spec.rb
@@ -8,4 +8,9 @@ describe 'logrotate::default' do
     it { should be_mode(644) }
     it { should contain('include /etc/logrotate.d') }
   end
+
+  describe file('/etc/logrotate.d') do
+    it { should be_a_directory }
+    it { should be_mode(755) }
+  end
 end
