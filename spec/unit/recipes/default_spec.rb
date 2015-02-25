baa2b5a784c9b07fc1e88d4de7b95615e6d752e2
Steven Danna <steve@chef.io>
2015-02-25 11:04:15 +0000

    Resolve Chefspec warnings by suing ChefSpec::SolorRunner

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe 'logrotate::default' do
-  let(:chef_run) { ChefSpec::Runner.new.converge(described_recipe) }
+  let(:chef_run) { ChefSpec::SoloRunner.new.converge(described_recipe) }
 
   it 'installs the logrotate package' do
     expect(chef_run).to install_package('logrotate')
