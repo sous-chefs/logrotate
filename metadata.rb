e0edc976578a560efe2611e58e0e409593f469c4
Steven Danna <steve@chef.io>
2015-02-25 11:06:57 +0000

    Update metadata.rb (maintainer, maintainer_email, and long_description)
    The long_description is returned by the server to chef-client.  This
    is wasteful and unhelpful.

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,9 @@
 name              'logrotate'
-maintainer        'Chef Software, Inc.'
-maintainer_email  'cookbooks@chef.io'
+maintainer        'Steven Danna'
+maintainer_email  'steve@chef.io'
 license           'Apache 2.0'
 description       'Installs logrotate package and provides a definition for logrotate configs'
-long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+long_description  'Installs the logrotate package, manages /etc/logrotate.conf, and provides a logrotate_app definition.'
 version           '1.8.0'
 
 recipe 'logrotate', 'Installs logrotate package'
