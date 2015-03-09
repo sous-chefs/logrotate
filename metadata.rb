e81a50a438d1783cb6c585bfae212476ce30b9e3
Steven Danna <steve@opscode.com>
2015-03-09 11:31:32 +0000

    Release v1.9.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email  'steve@chef.io'
 license           'Apache 2.0'
 description       'Installs logrotate package and provides a definition for logrotate configs'
 long_description  'Installs the logrotate package, manages /etc/logrotate.conf, and provides a logrotate_app definition.'
-version           '1.8.0'
+version           '1.9.0'
 
 recipe 'logrotate', 'Installs logrotate package'
 provides 'logrotate_app'
