sous-chefs/logrotate

commit 8484c951a666c6f6ff674631ee3473ea55525f3c

Add support for the AIX platform.
This commit adds the support necessary for platforms that do not already
have the logrotate package (or may need to install the package from
source). Additionally, all attributes can be driven from an external
Policyfile or cookbook.

recipes/default.rb
@@ -3,6 +3,7 @@
 # Recipe:: default
 #
 # Copyright 2009-2013, Chef Software, Inc.
+# Copyright 2016, Bloomberg Finance L.P.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,20 +17,25 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+return if platform?('windows')
 
-package "logrotate"
-
-directory "/etc/logrotate.d" do
-  owner "root"
-  group "root"
-  mode "0755"
-  action :create
+package node['logrotate']['package']['name'] do
+  provider node['logrotate']['package']['provider'] if node['logrotate']['package']['provider']
+  source node['logrotate']['package']['provider'] if node['logrotate']['package']['source']
+  version node['logrotate']['package']['provider'] if node['logrotate']['package']['version']
+  action :upgrade
 end
 
-if platform? "solaris2" # ~FC023 style preference
-  cron "logrotate" do
-    minute "35"
-    hour "7"
-    command "/usr/sbin/logrotate /etc/logrotate.conf"
-  end
+directory node['logrotate']['directory'] do
+  owner 'root'
+  group node['root_group']
+  mode '0755'
+end
+
+cron node['logrotate']['cron']['name'] do
+  minute node['logrotate']['cron']['minute']
+  hour node['logrotate']['cron']['hour']
+  command node['logrotate']['cron']['command']
+  not_if { platform_family?('debian') }
+  not_if { platform_family?('rhel') }
 end

attributes/default.rb
@@ -3,6 +3,7 @@
 # Attribute:: default
 #
 # Copyright 2013, Chef
+# Copyright 2016, Bloomberg Finance L.P.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,22 +18,36 @@
 # limitations under the License.
 #
 
-default["logrotate"]["global"] = {
-  "weekly" => true,
-  "rotate" => 4,
-  "create" => "",
+default['logrotate']['package'] = {
+  'name' => 'logrotate',
+  'source' => nil,
+  'version' => nil,
+  'provider' => nil
+}
 
-  "/var/log/wtmp" => {
-    "missingok" => true,
-    "monthly" => true,
-    "create" => "0664 root utmp",
-    "rotate" => 1,
+default['logrotate']['directory'] = '/etc/logrotate.d'
+
+default['logrotate']['cron']['name'] = 'logrotate'
+default['logrotate']['cron']['command'] = '/usr/sbin/logrotate /etc/logrotate.conf'
+default['logrotate']['cron']['minute'] = 35
+default['logrotate']['cron']['hour'] = 2
+
+default['logrotate']['global'] = {
+  'weekly' => true,
+  'rotate' => 4,
+  'create' => '',
+
+  '/var/log/wtmp' => {
+    'missingok' => true,
+    'monthly' => true,
+    'create' => '0664 root utmp',
+    'rotate' => 1
   },
 
-  "/var/log/btmp" => {
-    "missingok" => true,
-    "monthly" => true,
-    "create" => "0660 root utmp",
-    "rotate" => 1,
-  },
+  '/var/log/btmp' => {
+    'missingok' => true,
+    'monthly' => true,
+    'create' => '0660 root utmp',
+    'rotate' => 1
+  }
 }

LICENSE
@@ -186,7 +186,8 @@ APPENDIX: How to apply the Apache License to your work.
    same "printed page" as the copyright notice for easier
    identification within third-party archives.
 
-Copyright [yyyy] [name of copyright owner]
+Copyright 2009-2013, Chef Software, Inc.
+Copyright 2016, Bloomberg Finance L.P.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.

commit adb4b50265867c5a819dfb4edb3fe287f1a4fc9c

Use correct attributes in package resource
This issue was introduced in 8484c951.

Fixes #95

recipes/default.rb
@@ -22,8 +22,8 @@ return if platform?("windows")
 
 package node["logrotate"]["package"]["name"] do
   provider node["logrotate"]["package"]["provider"] if node["logrotate"]["package"]["provider"]
-  source node["logrotate"]["package"]["provider"] if node["logrotate"]["package"]["source"]
-  version node["logrotate"]["package"]["provider"] if node["logrotate"]["package"]["version"]
+  source node["logrotate"]["package"]["source"] if node["logrotate"]["package"]["source"]
+  version node["logrotate"]["package"]["version"] if node["logrotate"]["package"]["version"]
   action :upgrade
 end
 