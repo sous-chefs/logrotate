b2c763e9f0e568707a63ebf2bc76b4eb9914429f
Tim Smith <tsmith84@gmail.com>
2019-10-01 14:22:55 -0700

    Release 2.2.2
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer        'Chef Software, Inc.'
 maintainer_email  'cookbooks@chef.io'
 license           'Apache-2.0'
 description       'Installs logrotate package and provides a resource for managing logrotate configs'
-version           '2.2.1'
+version           '2.2.2'
 
 %w(amazon centos debian fedora redhat scientific solaris2 ubuntu suse opensuse opensuseleap).each do |platform|
   supports platform
