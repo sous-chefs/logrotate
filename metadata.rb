17e72ef036144750557582b6e00e6b938531fa39
Tim Smith <tsmith84@gmail.com>
2019-10-01 14:21:54 -0700

    Remove deprecated metadata fields
    These aren't necessary or used anymore
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -3,16 +3,12 @@ maintainer        'Chef Software, Inc.'
 maintainer_email  'cookbooks@chef.io'
 license           'Apache-2.0'
 description       'Installs logrotate package and provides a resource for managing logrotate configs'
-long_description  'Installs the logrotate package, manages /etc/logrotate.conf, and provides a logrotate_app resource.'
 version           '2.2.1'
-
-recipe 'logrotate', 'Installs logrotate package'
-provides 'logrotate_app'
 
 %w(amazon centos debian fedora redhat scientific solaris2 ubuntu suse opensuse opensuseleap).each do |platform|
   supports platform
 end
 
-source_url 'https://github.com/stevendanna/logrotate'
-issues_url 'https://github.com/stevendanna/logrotate/issues'
-chef_version '>= 12.5' if respond_to?(:chef_version)
+source_url 'https://github.com/chef-cookbooks/logrotate'
+issues_url 'https://github.com/chef-cookbooks/logrotate/issues'
+chef_version '>= 12.5'
