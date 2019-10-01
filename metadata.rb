9a34511a50663111cfe1898725f19980d6f98683
Tim Smith <tsmith84@gmail.com>
2019-10-01 14:21:33 -0700

    Switch the maintainer over to Chef Software
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name              'logrotate'
-maintainer        'Steven Danna'
-maintainer_email  'steve@chef.io'
+maintainer        'Chef Software, Inc.'
+maintainer_email  'cookbooks@chef.io'
 license           'Apache-2.0'
 description       'Installs logrotate package and provides a resource for managing logrotate configs'
 long_description  'Installs the logrotate package, manages /etc/logrotate.conf, and provides a logrotate_app resource.'
