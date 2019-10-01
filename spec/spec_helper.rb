2afae7ebd7029b126b31441ba0764ae12198ca22
Tim Smith <tsmith84@gmail.com>
2019-10-01 14:22:17 -0700

    Use Ubuntu 18.04 in the specs not 16.04
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,5 +6,5 @@ RSpec.configure do |config|
   config.formatter = :documentation # Use the specified formatter
   config.log_level = :error         # Avoid deprecation notice SPAM
   config.platform = 'ubuntu'
-  config.version = '16.04'
+  config.version = '18.04'
 end
