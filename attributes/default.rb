4bf0a00a2f626690c3e1d5b0b5a4a975d196fc29
Jeff Blaine <jblaine@mitre.org>
2020-06-03 16:29:26 -0400

    [ISSUE-138] - EL8 conflicts with default attrs
    * Introduces platform_family and version specific customizations
    to default attributes to stop a conflict on EL8 with the OS-
    provided /etc/logrotate.d/* files for wtmp and btmp.
    
    Signed-off-by: Jeff Blaine <jblaine@kickflop.net>

--- attributes/default.rb
+++ attributes/default.rb
@@ -35,22 +35,32 @@ default['logrotate']['cron']['command'] = '/usr/sbin/logrotate /etc/logrotate.co
 default['logrotate']['cron']['minute'] = 35
 default['logrotate']['cron']['hour'] = 2
 
-default['logrotate']['global'] = {
-  'weekly' => true,
-  'rotate' => 4,
-  'create' => '',
+if platform_family?('rhel')
+  if node['platform_version'].to_i >= 8
+    default['logrotate']['global'] = {
+      'weekly' => true,
+      'rotate' => 4,
+      'create' => '',
+    }
+  end
+else
+  default['logrotate']['global'] = {
+    'weekly' => true,
+    'rotate' => 4,
+    'create' => '',
 
-  '/var/log/wtmp' => {
-    'missingok' => true,
-    'monthly' => true,
-    'create' => '0664 root utmp',
-    'rotate' => 1,
-  },
+    '/var/log/wtmp' => {
+      'missingok' => true,
+      'monthly' => true,
+      'create' => '0664 root utmp',
+      'rotate' => 1,
+    },
 
-  '/var/log/btmp' => {
-    'missingok' => true,
-    'monthly' => true,
-    'create' => '0600 root utmp',
-    'rotate' => 1,
-  },
-}
+    '/var/log/btmp' => {
+      'missingok' => true,
+      'monthly' => true,
+      'create' => '0600 root utmp',
+      'rotate' => 1,
+    },
+  }
+end
