15a4b9ed4850d95ca8bea375aaee031f6e772d3f
rjhornsby <rhornsby@ktzr.net>
2022-09-21 11:36:39 -0500

    fix windows.
    they say you can't fix stupid, so this might all be tilting at windmills.

--- resources/package.rb
+++ resources/package.rb
@@ -34,16 +34,29 @@ action_class do
 
   def do_windows_install
 
+    powershell_script 'install_nuget_provider' do
+      code 'Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force'
+      # windows ---- i hate you so much.
+      action :run
+      not_if 'Get-PackageProvider -Name NuGet'
+    end
+
+    powershell_package_source 'PowershellGallery' do
+      source_location 'https://www.powershellgallery.com/api/v2'
+      notifies_before :run, 'powershell_script[install_nuget_provider]'
+    end
+
+
     begin
       # there's a bug in cinc 17.10.0, fixed in > 17.10.0 but as of this writing
       # 17.10.0 is the latest version of cinc client available.
       #
       # The package installs fine, but the bug causes a problem that makes chef think
       # the powershell command failed. begin/rescue here hacks around the problem.
-      powershell_package 'Log-Rotate' do
-        skip_publisher_check true
-        action :install
-      end
+      # powershell_package 'Log-Rotate' do
+      #   skip_publisher_check true
+      #   action :install
+      # end
     rescue NoMethodError => e
       msg = <<EOF
         Chef just did an amber heard on the bed:
@@ -105,8 +118,6 @@ action :install do
   end
 
   do_package_action :install
-
-  2.times { |t| file lr_basepath("/var/log/dummy_log_#{t}") }
 
 end
 
