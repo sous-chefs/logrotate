31ed1b3903f2bf7c50564f13680afbbd856df4d0
rjhornsby <rhornsby@ktzr.net>
2023-09-07 14:01:38 -0500

    fix checksum

--- attributes/7z.rb
+++ attributes/7z.rb
@@ -1,2 +1,2 @@
 default['sev1-logrotate']['7z']['version'] = '23.01'
-default['sev1-logrotate']['7z']['checksum'] = '26cb6e9f56333682122fafe79dbcdfd51e9f47cc7217dccd29ac6fc33b5598cd'
+default['sev1-logrotate']['7z']['checksum'] = '0ba639b6dacdf573d847c911bd147c6384381a54dac082b1e8c77bc73d58958b'
